21e46badfcaebaacc58c09d994cf0785b19e1726
Rai Umer <raifarooq7860786@gmail.com>
2024-04-04 01:47:02 +0500

    post

--- ejs part/server.js
+++ ejs part/server.js
@@ -1,43 +1,57 @@
-var express=require("express");
-var cors=require("cors");
-var path=require('path');
-let bodyParser=require("body-parser")
-const corsOption = {
-    origin:'*',
-    optionSuccessStatus: 200
-};
+let express=require("express");
 let user = [
     {ein: "raifarooq7860786@gmail.com", pin: "121212"}
 ];
+let ep="cal";
 const port = 3001;
-var app = express()
-app.use(bodyParser());
-app.use(cors());
-app.set('views', path.join(__dirname+"views"));
+var app = express();
 app.set('view engine','ejs');
+app.use(express.static('views' ));
 
-
-
+app.get(`/${ep}`,(req,res) => {
+    res.render('calculator.ejs');
+})
 
 app.get("/",(req, res) => {
-    res.render('index.ejs')
+    let af= 'Basit';
+    res.render('index.ejs',{em: af});
 })
-// app.get("/login",cors(corsOption),(req, res) => {
-//     let email=req.query.ein;
-//   let pass=req.query.pin;
-//     let ch1=0;+6
-//     user.forEach(el => {
-//         if(el.ein==email && el.pin==pass){
-//             ch1=1;
-//         }
-//     });
-//     if(ch1==1){
-//         res.send("successfully logined");
-//     }
-//     else{
-//         res.send("failed");
-//     }
-// })
+app.get("/login",(req, res) => {
+    let email=req.query.ein;
+  let pass=req.query.pin;
+    let ch1=0;+6
+    user.forEach(el => {
+        if(el.ein==email && el.pin==pass){
+            ch1=1;
+        }
+    });
+    if(ch1==1){
+        res.send("successfully logined");
+    }
+    else{
+        res.send("failed");
+    }
+})
+app.post("/login",(req, res) => {
+    let email=req.query.ein;
+  let pass=req.query.pin;
+    let ch1=0;+6
+    user.forEach(el => {
+        if(el.ein==email && el.pin==pass){
+            ch1=1;
+        }
+    });
+    if(ch1==1){
+        res.send("successfully logined");
+    }
+    else{
+        res.send("failed");
+    }
+})
+app.get('/final', (req,res)=>{
+    let value=["Huzaifa","login"];
+    res.render('index.ejs',{em:value})
+})
 app.listen(port, () => {
     console.log(`Server started on port: http://localhost:${port}`);
 })
