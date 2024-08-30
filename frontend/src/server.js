3d6f6b487b692c50d6cd2efd6d5d76c6153a181d
Rai Umer <raifarooq7860786@gmail.com>
2024-08-30 05:19:37 +0500

    web

---
+++ frontend/src/server.js
@@ -0,0 +1,7 @@
+// export const server = "http://10.54.4.220:8000/api/v2";
+
+// export const backend_url = "http://10.54.4.220:8000/";
+
+export const server = "http://localhost:8000/api/v2";
+
+export const backend_url = "http://localhost:8000/";
